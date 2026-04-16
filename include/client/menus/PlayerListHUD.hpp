0d9050277f730659409a3307251a266cf9d89b3c
Lucas <lucasdominique99@gmail.com>
2026-04-16 18:14:14 +0200

    Players connected HUD

---
+++ include/client/menus/PlayerListHUD.hpp
@@ -0,0 +1,32 @@
+
+#ifndef PLAYERLISTHUD_HPP
+#define PLAYERLISTHUD_HPP
+
+#include "Menu.hpp"
+#include <vector>
+#include <cstdint>
+
+struct PlayerEntry {
+    uint32_t id;
+    bool     isLocal;
+    float    pingMs;   // -1 = unknown, >=0 = known
+};
+
+class PlayerListHUD : public Menu {
+public:
+    PlayerListHUD(float width, float height);
+
+    void update(const std::vector<PlayerEntry>& players);
+    void resize(float width, float height) override;
+
+protected:
+    void build() override;
+    void onRender() override;
+
+private:
+    std::vector<PlayerEntry> m_players;
+
+    static glm::vec3 idToColor(uint32_t id);
+};
+
+#endif // PLAYERLISTHUD_HPP
